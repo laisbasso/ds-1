8423117362dbf7fa968b2df5dbced6a756e48f5f
Lais <laisbasso@live.com>
2020-10-23 10:10:40 -0300

    add tarefas pweb

--- PWEB/Aprovação/script.js
+++ PWEB/Aprovação/script.js
@@ -1,10 +1,19 @@
-function exibir(){
-    var n1 = parseInt(document.getElementById("n1").value)
-    var n2 = parseInt(document.getElementById("n2").value)
-    var n3 = parseInt(document.getElementById("n3").value)
+function exibir() {
+  var n1 = parseInt(document.getElementById("n1").value);
+  var n2 = parseInt(document.getElementById("n2").value);
+  var n3 = parseInt(document.getElementById("n3").value);
+  var faltas = parseInt(document.getElementById("faltas").value);
 
-    var media = (n1 + n2 + n3) / 3
-    var msg = (media >= 7) ? "Aprovado" : "Reprovado"
+  var media = (n1 + n2 + n3) / 3;
+  var frequencia = 100 - faltas
 
-    alert(msg)
+  if (media >= 7 && frequencia > 75) {
+    alert("Aprovado");
+    document.getElementById("Aprovado").style.display = "inline";
+    document.getElementById("Reprovado").style.display = "none";
+  } else {
+    alert("Reprovado");
+    document.getElementById("Aprovado").style.display = "none";
+    document.getElementById("Reprovado").style.display = "inline";
+  }
 }

63d8f06c99e43513d80c473802faa22cba402f2f
Lais <laisbasso@live.com>
2020-11-11 23:02:39 -0300

    add tarefas C

---
+++ C/mediaPositivos.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	float media, soma = 0;
+	int n = 1, cont = 0;
+	
+    do {
+        printf("Entre com um valor positivo. Digite 0 ou um valor negativo para encerrar: ");
+        scanf("%d", &n);
+
+        if (n > 0){
+            soma += n;
+            cont++;
+        }
+    } while (n > 0);
+    
+    media = soma / cont;
+
+    printf("A média dos valores positivos é %.2f", media);
+        
+    return 0;
+}

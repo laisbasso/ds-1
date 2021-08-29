d12c2df9a48e9d1fb5ab48b6dec198c32d10fa30
Lais Basso <laisbasso@live.com>
2021-08-29 16:08:31 -0300

    add tarefas

--- Módulo-3/BD/DBLivraria_Procedure_2.sql
+++ Módulo-3/BD/DBLivraria_Procedure_2.sql
@@ -4,7 +4,7 @@ use DBLivraria2sem2021;
 
 -- Criar uma procedure – p_gravar_livro – com opção para incluir e alterar os registros na tabela LIVRO.
 
-create procedure p_gravar_livro -- incluir ok, falta alterar (var opcional?)
+create procedure p_gravar_livro
 @isbnidliv char(15),
 @tituloliv varchar(100),
 @resumoliv varchar(250),
@@ -15,12 +15,31 @@ create procedure p_gravar_livro -- incluir ok, falta alterar (var opcional?)
 @vlvendaliv numeric(10,2)
 as
 begin
+	declare @isbnexiste int
+
+	select @isbnexiste = 1 from livro where isbn_id_liv = @isbnidliv;
+
+	if @isbnexiste = 1
+		update livro set 
+			isbn_id_liv = @isbnidliv,
+			titulo_liv = @tituloliv,
+			resumo_liv = @resumoliv, 
+			codigo_id_for = @codigoidfor,
+			codigo_id_ed = @codigoided, 
+			dtpublicacao_liv = @dtpublicacaoliv,
+			vlcusto_liv = @vlcustoliv, 
+			vlvenda_liv = @vlvendaliv
+		where isbn_id_liv = @isbnidliv
+
+	else
 	insert into livro
-				(isbn_id_liv, titulo_liv, resumo_liv, codigo_id_for, codigo_id_ed, dtpublicacao_liv, vlcusto_liv, vlvenda_liv)
-	values (@isbnidliv, @tituloliv, @resumoliv, @codigoidfor, @codigoided, @dtpublicacaoliv, @vlcustoliv, @vlvendaliv)
+					(isbn_id_liv, titulo_liv, resumo_liv, codigo_id_for, codigo_id_ed, dtpublicacao_liv, vlcusto_liv, vlvenda_liv)
+		values (@isbnidliv, @tituloliv, @resumoliv, @codigoidfor, @codigoided, @dtpublicacaoliv, @vlcustoliv, @vlvendaliv)
 end;
 
-select * from livro;
+execute p_gravar_livro '8533613370', 'O senhor dos anéis I - Edição Limitada', 'Térra Média', 1, 13, '2010-02-03', 30, 50; 
+
+select * from livro where isbn_id_liv = '8533613370';    
 
 -- Criar uma procedure – p_ins_livro_completo – para inserir o registro na tabela LIVRO, CATEGORIA e AUTOR.
 
@@ -41,7 +60,7 @@ create procedure p_ins_livro_completo
 @dtnascimentoaut date
 as
 begin
-	insert into categoria (descricao_cat) value (@descricaocat);
+	insert into categoria (descricao_cat) values (@descricaocat);
 
 	insert into autor (nome_aut, dtnascimento_aut) values (@nomeaut, @dtnascimentoaut);
 
@@ -50,27 +69,55 @@ begin
 	values (@isbnidliv, @tituloliv, @resumoliv, @codigoidfor, @codigoided, @dtpublicacaoliv, @vlcustoliv, @vlvendaliv)
 end;
 
--- Criar uma procedure – p_ins_carrrinho_item – Para inserir registro na tabela CARRINHO e tabela ITEM_CARRINHO, calculando o valor total do item do carrinho, após pesquisa na tabela LIVRO.
+execute p_ins_livro_completo '8533613374', 'A Mansão Hollow', 'Detetive Poirot', 1, 2, '2000-06-26', 36, 67, 'Suspense', 'Agatha Christie', '15-07-1890';
+
+select * from livro where isbn_id_liv = '8533613374';    
+select * from categoria where descricao_cat = 'Suspense';    
+select * from autor where nome_aut = 'Agatha Christie';   
+
+-- Criar uma procedure – p_ins_carrrinho_item – Para inserir registro na tabela CARRINHO e tabela ITEM_CARRINHO, 
+-- calculando o valor total do item do carrinho, após pesquisa na tabela LIVRO.
 
 select * from carrinho;
+select * from item_carrinho;
+select * from cliente;
+
+SET IDENTITY_INSERT carrinho ON;
 
 create procedure p_ins_livro_completo 
-
+@numeroidcar int,
 @loginidcli varchar(10),
-@datapedidocar datetime, 
-@dataentregacar datetime, 
 @enderecoentregacar varchar(60), 
 @complementoentregacar varchar(15),
 @bairroentregacar varchar(20), 
 @cidadeentregacar varchar(30), 
 @ufidest char(2), 
 @codigoidpag int, 
-@codigoidfun int
+@codigoidfun int,
+@isbnidliv char(15),
+@qtdadeitem int,
+@situacaoitem char(1)
 as
 begin
 
-insert into carrinho (login_id_cli, data_pedido_car, data_entrega_car, endereco_entrega_car, complemento_entrega_car,
-							bairro_entrega_car, cidade_entrega_car, uf_id_est, codigo_id_pag, codigo_id_fun)
-values (@loginidcli, @datapedidocar, @dataentregacar, @enderecoentregacar, @complementoentregacar,
-							@bairroentregacar, @cidadeentregacar, @ufidest, @codigoidpag, @codigoidfun)
+	declare @datapedidocar date = cast(getdate() as date);
+	declare @dataentregacar date = cast(getdate() + 5 as date);
+	declare @vlvendaliv numeric(10,2)
+	declare @vltotalitem numeric(10,2)
+	
+	insert into carrinho (numero_id_car, login_id_cli, data_pedido_car, data_entrega_car, endereco_entrega_car, complemento_entrega_car,
+								bairro_entrega_car, cidade_entrega_car, uf_id_est, codigo_id_pag, codigo_id_fun)
+	values (@numeroidcar, @loginidcli, @datapedidocar, @dataentregacar, @enderecoentregacar, @complementoentregacar,
+								@bairroentregacar, @cidadeentregacar, @ufidest, @codigoidpag, @codigoidfun)
+
+	select @vlvendaliv = vlvenda_liv from livro where isbn_id_liv = @isbnidliv
+
+	select @vltotalitem = @vlvendaliv * @qtdadeitem
+
+	insert into item_carrinho (isbn_id_liv, numero_id_car, qtdade_item, vltotal_item, situacao_item)
+		values (@isbnidliv, @numeroidcar, @qtdadeitem, @vltotalitem, @situacaoitem);
 end;
+
+drop procedure p_ins_livro_completo
+
+execute p_ins_livro_completo 1, 'lais', 'Rua', 'Apto', 'Bairro', 'SP', 'SP', 1, 1, '8533613374', 2, 'D';
